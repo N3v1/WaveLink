0d95ffa159cd0ae7d0974fb0636ec17ad779c84b
N3v1 <n3v1010@gmail.com>
2024-05-20 16:49:57 +0200

    Initial Commit

---
+++ WaveLink/WaveLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveLinkApp.swift
+//  WaveLink
+//
+//  Created by Nevio Hirani on 20.05.24.
+//
+
+import SwiftUI
+
+@main
+struct WaveLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
