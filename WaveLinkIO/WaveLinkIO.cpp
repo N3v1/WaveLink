722490882f72715eeaa9742b2c2968d0fa4a6d96
N3v1 <n3v1010@gmail.com>
2024-05-20 22:26:25 +0200

    (feat): Config

---
+++ WaveLinkIO/WaveLinkIO.cpp
@@ -0,0 +1,15 @@
+/* add your code here */
+
+#include "WaveLinkIO.hpp"
+
+//kern_return_t VirtualAudioDevice::Start(IOService *provider) {
+//    kern_return_t result = super::Start(provider);
+//    if (result != kIOReturnSuccess) {
+//        return result;
+//    }
+//    
+//    ConfigureAudioEngine();
+//    
+//    this->isStarted = true;
+//    return kIOReturnSuccess;
+//}
